dc8690d8e81aa60ab1b2ff9c4aecd58ff5c15462
ludazefer <56082823+ludazefer@users.noreply.github.com>
2019-10-03 11:03:56 +0300

    Update HelloWord.java

--- src/bacics/HelloWord.java
+++ src/bacics/HelloWord.java
@@ -4,6 +4,7 @@ public class HelloWord {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("adding code");
+		System.out.println("adding code from git");
 
 	}
 
